7b00957dacc43ae12398948ef1167160056605e6
ItsAltoo <Itsmalik57@gmail.com>
2024-12-18 13:51:33 +0800

    18.12.24

--- Testing/py28.py
+++ Testing/py28.py
@@ -58,7 +58,7 @@ print('\n',13*' </line> ','\n')
     
 from abc import ABC, abstractmethod
 
-class Shape(ABC):
+class Shape(ABC):   # Abstract Base Class
     @abstractmethod
     def area(self):
         pass
@@ -67,7 +67,7 @@ class Shape(ABC):
     def perimeter(self):
         pass
 
-class Rectangle(Shape):
+class Rectangle(Shape):     # Implementasi abstrak
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -83,6 +83,44 @@ rect = Rectangle(4, 5)
 print(rect.area())        # Output: 20
 print(rect.perimeter())   # Output: 18
 
+
+
+
+
+class Shape2(ABC):  # Abstract Base Class
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle2(Shape2):  # Implementasi abstrak
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+class Rectangle2(Shape2):  # Implementasi abstrak
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+# Menggunakan kelas abstrak
+shapes = [Circle2(5), Rectangle2(4, 6)]
+for shape in shapes:
+    print(f"Area: {shape.area()}, Perimeter: {shape.perimeter()}")
 
 
 
---
+++ Testing/py29.py
@@ -0,0 +1,57 @@
+import os 
+os.system('cls')
+
+# Getter & Setter
+
+class Character :
+    
+    def __init__(self,name,darah,power):
+        self.nama = name
+        self.__darah = darah
+        self.__power = power
+        #self.__info = 'name : {} , Darah : {}'.format(self.nama,self.__darah)
+    
+    @property # <- decorator @property
+    def informasi(self):
+        return 'name : {} , Darah : {}'.format(self.nama,self.__darah)
+    
+    @property
+    def power(self):
+        pass
+    
+    @power.getter
+    def power(self):
+        return self.__power
+    
+    @power.setter
+    def power(self,inpt):
+        self.__power = inpt
+    
+    @power.deleter
+    def power(self):
+        print('delete power !')
+        self.__power = None
+    
+    
+    
+    
+    
+    
+    
+yuki = Character('yuki',100,20)
+
+# print(yuki.informasi()) 
+print(yuki.informasi)   # <- mengubah method menjadi atribut
+yuki.nama = 'yui'
+print(yuki.informasi)
+
+print('\n',13*' </line> ','\n')
+
+print(yuki.power)
+yuki.power = 80
+print(yuki.power)
+print(yuki.__dict__)
+
+del yuki.power
+
+print(yuki.__dict__)
