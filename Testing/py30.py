0d7fefce87741e5e5c388990d293d116dbb199f4
ItsAltoo <Itsmalik57@gmail.com>
2024-12-21 20:27:50 +0800

    21.12.24

--- Testing/py30.py
+++ Testing/py30.py
@@ -14,3 +14,25 @@ class Data(Mhs):
     
 yuki = Data('malik',2342,12)
 print(yuki.info)
+print(yuki.__dict__)
+
+# object.__dict__ = untuk mengetahui semua atribut yang ada di dalam object
+
+# decorator @property = untuk mengubah method menjadi seperti atribut
+
+# __init__ = untuk menginisialisasi object
+
+# super() = untuk mengakses method dari parent class
+
+# super().__init__() = untuk mengakses method __init__ dari parent class
+
+# super().__init__(self,nama,nim) = untuk mengakses method __init__ dari parent class dengan parameter nama dan nim
+
+# tuple
+tuple = (1,2,3,4,5)
+print(tuple[0])
+print(tuple[1])
+# tuple[0] = 2 
+# tuple[0] = 2 # error karena tuple tidak bisa diubah
+
+# 
