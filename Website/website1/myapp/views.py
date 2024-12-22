ad172af8e29ac71d9384d03f480a65ab42042047
ItsAltoo <Itsmalik57@gmail.com>
2024-12-22 16:47:05 +0800

    22.12.24

--- Website/website1/myapp/views.py
+++ Website/website1/myapp/views.py
@@ -1,5 +1,6 @@
 # filepath: /C:/Users/itsma/OneDrive/Documents/Project/Belajar/Python/Website/myproject1/myapp/views.py
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Hello, world. You're at the home page.")
@@ -9,3 +10,6 @@ def about(request):
 
 def contact(request):
     return HttpResponse('<button style="background-color: blue; color: white;">Contact Us</button>')
+
+def hello_world(request):
+    return render(request,'myapp/hello.html')
--- Website/website1/myproject/urls.py
+++ Website/website1/myproject/urls.py
@@ -20,7 +20,8 @@ from myapp import views  # Import views from myapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Add a URL pattern for the home view
+    #path('', views.home, name='home'),  # Add a URL pattern for the home view
     path('about/', views.about, name='about'),  # Add a URL pattern for the about view
     path('contact/', views.contact, name='contact'),  # Add a URL pattern for the contact view
+    path('hello/', views.hello_world, name='hello_world'),  # Add a URL pattern for the hello_world view
 ]
