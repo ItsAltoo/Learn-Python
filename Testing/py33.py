8dc857572d00205ad08766cf811c56ff7899f4cc
ItsAltoo <Itsmalik57@gmail.com>
2024-12-28 18:12:40 +0800

    28.12.24

--- Testing/py33.py
+++ Testing/py33.py
@@ -22,3 +22,45 @@ d.show()
 # Karena method resolution order (MRO) adalah urutan pengecekan class yang diwarisi oleh class anak urutan pengecekan class adalah dari kiri ke kanan dan class B lebih dulu dari class C 
 
 print('=====================================')
+
+
+
+# magic method
+
+class Mhs:
+    
+    def __init__(self,nama,nim):
+        self.nama = nama
+        self.nim = nim
+        # biasa digunakan untuk inisialisasi object 
+        
+    def __str__(self):
+        return 'nama : {} , nim : {}'.format(self.nama,self.nim)
+    # biasa digunakan untuk menampilkan object dan biasa digunakan untuk user
+    
+    def __repr__(self):
+        return 'nama : {} , nim : {}'.format(self.nama,self.nim)
+    # biasa digunakan untuk menampilkan object dan biasa digunakan untuk debugging 
+    
+    def __add__(self,objek):
+        return self.nim + objek.nim
+    # biasa digunakan untuk operasi matematika dan operasi lainnya
+    
+    def __len__(self):
+        return len(self.nama)
+    # biasa digunakan untuk menghitung panjang object dan biasa digunakan untuk operasi matematika
+    
+    # def __del__(self):
+    #     print('object dihapus')
+    # biasa digunakan untuk menghapus object dan biasa digunakan untuk operasi lainnya
+
+mhs = Mhs('yuki',123)
+mhs2 = Mhs('yui',321)
+print(mhs)
+print(mhs + mhs2)
+print(mhs.__len__())
+print(mhs.__repr__())
+print(mhs.__str__())
+# del mhs
+
+print('=====================================')
