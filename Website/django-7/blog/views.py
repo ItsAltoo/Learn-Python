482ff738f6838709d581d07cd16c391b3a30d41c
ItsAltoo <Itsmalik57@gmail.com>
2025-01-06 21:27:28 +0800

    06.1.25

--- Website/django-7/blog/views.py
+++ Website/django-7/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Mahasiswa
 # Create your views here.
+
+
+def index(request):
+    mhs = Mahasiswa.objects.all()
+    context = {
+        'title':'Welcome',
+        'heading':'This Test Website',
+        'mahasiswa': mhs,
+    }
+    return render(request,'blog/index.html',context)
