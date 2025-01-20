6a29aa28b52295e8e758b09eda3b959d749b3357
ItsAltoo <Itsmalik57@gmail.com>
2025-01-20 11:39:40 +0800

    20.1.25

--- Test/numpys.py
+++ Test/numpys.py
@@ -2,7 +2,33 @@ import numpy as np
 
 a = np.array([[7,4,2,0,4],[1,2,4,5,5]])
 b = np.array([[7,4,2,0,4],[1,2,4,5,5]])
+c = np.array([4,3,1,5,1])
 
 count = a + b
 
 print(count)
+print(15*' - ')
+
+matriksA = np.ones([5,4])
+
+print(matriksA)
+print(15*'-')
+print(matriksA + 2)
+print(15*'-')
+print(f"jumlah array : {a.ndim}")
+print(f"ukuran array :{a.shape}")
+print(f"jumlah isi array atau jumlah elemen :{a.size}")
+print(f"type : {a.dtype}")
+
+print(15*'-')
+
+zerosA = np.zeros(2)
+print(zerosA)
+onesA = np.ones(2)
+print(onesA)
+empytA = np.empty(3)
+print(empytA)
+arrageA = np.arange(4)
+print(arrageA)
+arrageB = np.arange(0,20,5)
+print(arrageB)
