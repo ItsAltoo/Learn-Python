71a368515fc26a9806028953291b8dd2aa7284dd
ItsAltoo <Itsmalik57@gmail.com>
2025-01-21 09:15:11 +0800

    21.1.25

--- Website/django-7/blog/views.py
+++ Website/django-7/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import MahasiswaForm
 from .models import Mahasiswa
 # Create your views here.
 
@@ -10,5 +10,20 @@ def index(request):
         'title':'Welcome',
         'heading':'This Test Website',
         'mahasiswa': mhs,
+    }
+    return render(request,'blog/index.html',context)
+
+def addMahasiswa(request):
+    form = MahasiswaForm()
+    if request.method == 'POST':
+        form = MahasiswaForm(request.POST)
+        if form.is_valid():
+            form.save()
+    mhs = Mahasiswa.objects.all()
+    context = {
+        'title':'Add Mahasiswa',
+        'heading':'Add Mahasiswa',
+        'form': form,
+        'mhs': mhs,
     }
     return render(request,'blog/index.html',context)
