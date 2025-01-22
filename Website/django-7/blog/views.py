9d0c8590cab5d591327088d4a0c47e0a6e8e2cf9
ItsAltoo <Itsmalik57@gmail.com>
2025-01-22 11:31:56 +0800

    22.1.25

--- Website/django-7/blog/views.py
+++ Website/django-7/blog/views.py
@@ -13,17 +13,17 @@ def index(request):
     }
     return render(request,'blog/index.html',context)
 
+
 def addMahasiswa(request):
     form = MahasiswaForm()
     if request.method == 'POST':
         form = MahasiswaForm(request.POST)
         if form.is_valid():
             form.save()
-    mhs = Mahasiswa.objects.all()
     context = {
         'title':'Add Mahasiswa',
         'heading':'Add Mahasiswa',
         'form': form,
-        'mhs': mhs,
+        'mhs': Mahasiswa.objects.all(),
     }
     return render(request,'blog/index.html',context)
