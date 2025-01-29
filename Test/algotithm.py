e279ac75631373ff8a355f11db93c00d6e983c5d
ItsAltoo <Itsmalik57@gmail.com>
2025-01-29 15:45:21 +0800

    29.1.25

--- Test/algotithm.py
+++ Test/algotithm.py
@@ -3,5 +3,32 @@
 num1 = 0
 num2 = 1
 
+print(num1)
+print(num2)
 for i in range(19):
+    newnum = num1 + num2
+    print(newnum)
+    num1 = num2
+    num2 = newnum
     
+print(20*'- ')
+
+def F(n):
+    if n <= 1:
+        return n
+    else:
+        return F(n - 1) + F(n - 2)
+
+print(F(19))
+    
+print(20*'- ')
+#DSA Array
+
+my_array = [2,6,3,1,5,7]
+nilai_awal = my_array[0]
+
+for i in my_array:
+    if i < nilai_awal:
+        nilai_awal = i
+        
+print(nilai_awal)
