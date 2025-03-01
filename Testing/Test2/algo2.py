392616269eeb53fba58816749d8646f15e1d4847
ItsAltoo <Itsmalik57@gmail.com>
2025-03-01 20:44:01 +0800

    1.3.25

--- Testing/Test2/algo2.py
+++ Testing/Test2/algo2.py
@@ -21,3 +21,47 @@ for i in range(n-1):
             my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
 
 print("Sorted array:", my_array)
+
+print('-'*8)
+
+for i in range(1,10):
+    if i % 2 != 0:
+        print(i)
+        
+print('-'*8)
+
+def luas_persegi(sisi):
+    return sisi **2
+
+print(luas_persegi(5))
+
+print('-'*8)
+
+# num = int(input('Masukkan Angka :'))
+
+# if num < 0 :
+#     print(f"Bilangan negatif {num}")
+# elif num > 0:
+#     print(f"Bilangan Positif {num}")
+# else:
+#     print("Bilangan Nol")
+    
+print('-'*8)
+
+myList = [1,2,3,4,5]
+total = 0
+
+for i in myList:
+    total += i
+    
+print(total)
+
+print('-'*8)
+
+name = input('Input Name :')
+tukar = ''
+
+for i in name:
+    tukar = i + tukar
+    
+print(tukar)
